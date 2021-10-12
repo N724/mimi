0fb36b58f6f5f4db590c6e6f7e318ff663a0c203
Ardor-Zhang <ardor-zhang@outlook.com>
2021-10-12 16:12:07 +0800

    feat: log

--- utils/setTime.js
+++ utils/setTime.js
@@ -3,12 +3,15 @@ function getProductFileList(todo, interval){
   setTimeout(getProductFileList, interval)
 }
 
-function setRegular(todo=() => {}, targetTime={ hour: 0, minute: 0, second: 0 }, interval=24 * 3600 * 1000){
+function setRegular(todo=() => {}, targetTime={ hour: 0, minute: 0, second: 0 }, interval=24 * 3600){
   const nowTime = new Date();
   const nowSeconds = nowTime.getHours() * 3600 + nowTime.getMinutes() * 60 + nowTime.getSeconds();
   const targetSeconds = targetTime.hour * 3600 + targetTime.minute * 60 + targetTime.second;
-  const timeInterval = targetSeconds > nowSeconds ? targetSeconds - nowSeconds: targetSeconds + 24 * 3600 - nowSeconds;
-  setTimeout(() => getProductFileList(todo, interval), timeInterval * 1000);
+  const timeInterval = targetSeconds > nowSeconds ? targetSeconds - nowSeconds: targetSeconds + interval - nowSeconds;
+  setTimeout(() => { 
+    getProductFileList(todo, interval * 1000);
+    console.log(`现在是${targetTime.hour}:${targetTime.minute}`);
+  }, timeInterval * 1000);
 };
   
 module.exports = setRegular;
